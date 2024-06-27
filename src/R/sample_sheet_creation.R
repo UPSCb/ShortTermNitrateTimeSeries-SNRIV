8074f2017578a813383e97a9c229f754a2f63990
nicolasDelhomme <nicolas.delhomme@umu.se>
2024-06-27 23:22:36 +0200

    Corrected a typo and extended the git ignore

--- src/R/sample_sheet_creation.R
+++ src/R/sample_sheet_creation.R
@@ -1,2 +1,5 @@
 files<-list.files("data/raw", pattern="R1_001.fastq.gz", full.names=TRUE)
-readr::write_csv(tibble::tibble(sample=sub("_S.*","",basename(files)),fastq_1=files,fastg_2=sub("R1","R2",files),strandedness="auto"),file="doc/sample_sheet.csv")
+readr::write_csv(tibble::tibble(sample=sub("_S.*","",basename(files)),
+                                fastq_1=files,
+                                fastq_2=sub("R1","R2",files),
+                                strandedness="auto"),file="doc/sample_sheet.csv")
