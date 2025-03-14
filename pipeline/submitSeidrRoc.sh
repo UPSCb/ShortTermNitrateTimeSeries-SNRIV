b1246f0e9b8554a89ecd169c5053eab1022af182
schoudhary <schoudhary@aspseq.upsc.se>
2025-03-14 14:39:43 +0100

    - restored the original runSeidrRoc with negative gold standard

--- pipeline/submitSeidrRoc.sh
+++ pipeline/submitSeidrRoc.sh
@@ -4,15 +4,13 @@ account=u2022003
 
 # process the argument
 pgs=/mnt/reference/goldStandard/Potra02_KEGG-based-positive-gold-standard.tsv
-# The ngs is optional, I will try running without this as well
-# ngs=/mnt/reference/goldStandard/Potra02_KEGG-based-negative-gold-standard.tsv
+ngs=/mnt/reference/goldStandard/Potra02_KEGG-based-negative-gold-standard.tsv
 # example: 
 # pgs=$(realpath ../goldStandard/Picea-abies_KEGG-based-positive-gold-standard.tsv)
 # ngs=$(realpath ../goldStandard/Picea-abies_KEGG-based-negative-gold-standard.tsv)
 
 bb=$(realpath data/seidr/results/aggregate/aggregated.sf)
 indir=$(realpath data/seidr/results/backbone)
-# out=$(realpath data/seidr/results/roc)
 out=$(realpath data/seidr/results/noNegGoldStdForRoc)
 singularity=$(realpath singularity/seidr_0.14.2.sif)
 
@@ -25,25 +23,14 @@ if [ ! -h $indir/$bbnam ]; then
   ln -sf -t $indir $bb 
 fi
 
-# find the network files
-# Running with the negative gold standard
-# for f in $(find $indir -name "*.sf"); do
-#   fnam=$(basename ${f/.sf/})
-# 
-#   # run the roc on all
-#   sbatch -A $account --mail-user=$email \
-#   -o $out/${fnam}_roc.out -e $out/${fnam}_roc.err \
-#   pipeline/runSeidrRoc.sh $singularity $f $pgs $ngs \
-#   $out/${fnam}_roc.tsv
-# done
-
-# Running without the negative gold standard
+find the network files
+Running with the negative gold standard
 for f in $(find $indir -name "*.sf"); do
   fnam=$(basename ${f/.sf/})
 
   # run the roc on all
-  sbatch -A $account \
+  sbatch -A $account --mail-user=$email \
   -o $out/${fnam}_roc.out -e $out/${fnam}_roc.err \
-  pipeline/runSeidrRoc.sh $singularity $f $pgs "" \
+  pipeline/runSeidrRoc.sh $singularity $f $pgs $ngs \
   $out/${fnam}_roc.tsv
 done
