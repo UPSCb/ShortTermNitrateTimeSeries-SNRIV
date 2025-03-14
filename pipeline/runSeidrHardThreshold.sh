c548d97eaa9f41051eb8e35c2b3a2a6418d242c6
EdoardoPiombo <edoardo.piombo@gmail.com>
2025-03-14 16:27:42 +0100

    Added hard threhshold script and modified submitSeidrRoc to run with and without the negative gold standard

---
+++ pipeline/runSeidrHardThreshold.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH -t 4:00:00
+#SBATCH -p main -n 2
+
+set -ex
+
+# variables
+CPU=2
+
+# helper functions
+source ${SLURM_SUBMIT_DIR:-$(pwd)}/UPSCb-common/src/bash/functions.sh
+
+# usage
+USAGETXT=\
+"
+  Usage: $0 <seidr file> <output folder>
+  
+"
+
+# sanity
+
+
+if [ $# -ne 3 ]; then
+  abort "This script expects 3 arguments"
+fi
+
+if [ ! -f $1 ]; then
+  abort "The first argument needs to be an existing singularity container"
+fi
+
+if [ ! -f $2 ]; then
+  abort "The second argument needs to be an existing network file"
+fi
+
+if [ ! -d $(dirname $3) ]; then
+  abort "The third argument directory needs to exist"
+fi
+
+# run
+export OMP_NUM_THREADS=$CPU
+singularity exec -B /mnt:/mnt $1 seidr threshold --in-file $2 -m 0.1 -M 0.9 -o $3
+
---
+++ pipeline/submitSeidrHardThreshold.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#singularity container
+singularity=$(realpath singularity/seidr_0.14.2.sif)
+account="u2024010"
+
+# process the argument
+input="/mnt/picea/projects/aspseq/htuominen/ShortTermNitrateTimeSeries-SNRIV/seidr/results/aggregate/aggregated.sf"
+out="/mnt/picea/projects/aspseq/htuominen/ShortTermNitrateTimeSeries-SNRIV/seidr/results/HardThreshold"
+mkdir -p $out
+
+# submit
+sbatch -A $account -e $out/thresholded.err -o $out/thresholded.out \
+  -J thresholdedLD1 pipeline/runSeidrHardThreshold.sh $singularity $input $out/thresholded.tsv
