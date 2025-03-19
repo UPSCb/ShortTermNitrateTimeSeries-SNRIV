425b7a126b94e1b3d659383a63ce456e8101518f
EdoardoPiombo <edoardo.piombo@gmail.com>
2025-03-19 17:08:13 +0100

    Successfully run the seidrRoc.R script

--- src/R/seidrRoc.R
+++ src/R/seidrRoc.R
@@ -64,7 +64,7 @@ plotRoc <- function(f){
 #' ```{R CHANGEME1, echo=FALSE, eval=FALSE}
 #' Change the path to the aggregated ROC results, if required. That ROC file must have been created using seidr roc -a option
 #' ```
-res <- plotRoc(here("data/seidr/results/ResRoc/backbone-10-percent_roc_WithNegative.tsv"))
+res <- plotRoc(here("data/seidr/results/ResRoc/aggregated_roc_WithNegative.tsv"))
 
 #' ### Stats of the gold standard analysis
 pander(res)
@@ -73,9 +73,10 @@ pander(res)
 #' ```{R CHANGEME1, echo=FALSE, eval=FALSE}
 #' Change the path to the backbone ROC results directory as well as the file matching patter,if required. 
 #' These ROC files must have been created using seidr roc -a option
+#' Doing the analysis only with the WithNegative files, maybe repeat it for the NoNegative
 #' ```
-files <- dir(here("data/seidr/roc"),pattern="backbone.*\\.tsv",full.names=TRUE)
-names(files) <- gsub("backbone-|_roc.tsv","",basename(files))
+files <- dir(here("data/seidr/results/ResRoc/"),pattern="backbone.*WithNegative\\.tsv",full.names=TRUE)
+names(files) <- gsub("backbone-|_roc_WithNegative.tsv","",basename(files))
 files <- files[order(as.integer(sub("-percent","",names(files))))]
 resb <- lapply(files,plotRoc)
 
@@ -117,6 +118,59 @@ paucs <- aucs * t(t(gsNum) / colMaxs(gsNum))
 pander(paucs)
 
 heatmap.2(paucs,trace="none",col=hpal,margins=c(7.1,7.1),Colv=FALSE,dendrogram="row")
+
+
+# Now repeat everything with NoNegatives.
+res <- plotRoc(here("data/seidr/results/ResRoc/aggregated_roc_NoNegative.tsv"))
+
+#' ### Stats of the gold standard analysis
+pander(res)
+
+files <- dir(here("data/seidr/results/ResRoc/"),pattern="backbone.*NoNegative\\.tsv",full.names=TRUE)
+names(files) <- gsub("backbone-|_roc_NoNegative.tsv","",basename(files))
+files <- files[order(as.integer(sub("-percent","",names(files))))]
+resb <- lapply(files,plotRoc)
+
+#' ### Stats of the gold standard (GS) analysis
+pander(resb)
+
+#' # Summary
+#' Report all AUCs
+aucs <- c(lapply(resb,select,c("ALGO","AUC")),aggregated=list(select(res,c("ALGO","AUC")))) %>% 
+  enframe %>% unnest(cols=c("value")) %>% pivot_wider(names_from=name,values_from=AUC) %>% 
+  column_to_rownames("ALGO") %>% as.matrix()
+
+pander(aucs)
+
+#' ## Heatmaps
+#' ### AUCs
+#' **NOTE** In the heatmap below, the only fair comparisons are to look
+#' across the `aggregated` column or the `irp` row. Any other comparisons,
+#' albeit possibly informative will be biased by the selection of a subset 
+#' of edges for a given algorithm, making them theoretically incomparable.
+#'
+heatmap.2(aucs,trace="none",col=hpal,margins=c(7.1,7.1))
+
+heatmap.2(aucs,trace="none",col=hpal,margins=c(7.1,7.1),Colv=FALSE,dendrogram="row")
+
+#' ### AUCs penalised by the number of Gold Standard (GS) edges used
+#' The rationale here is to check the effect of a limited number of GS on the 
+#' AUC calculation
+resb$aggregated=res
+
+gsNum <- lapply(resb,select,c("ALGO","TP","FP")) %>%
+  enframe %>% unnest(cols=c("value")) %>% 
+  mutate(TOTAL=TP+FP) %>% select(-c("TP","FP")) %>% 
+  pivot_wider(names_from=name,values_from=TOTAL) %>% 
+  column_to_rownames("ALGO") %>% as.matrix()
+
+paucs <- aucs * t(t(gsNum) / colMaxs(gsNum))
+
+pander(paucs)
+
+heatmap.2(paucs,trace="none",col=hpal,margins=c(7.1,7.1),Colv=FALSE,dendrogram="row")
+
+
 
 #' # Conclusion
 #' CHANGEME for some conclusion
