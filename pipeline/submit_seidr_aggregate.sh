687a0392436034cc3d2929bf23aa638c053ea11b
schoudhary <schoudhary@aspseq.upsc.se>
2025-03-19 10:14:13 +0100

    -changed to real pathname for the seidr-aggregate-micro

--- pipeline/submit_seidr_aggregate.sh
+++ pipeline/submit_seidr_aggregate.sh
@@ -3,6 +3,6 @@
 mkdir -p data/seidr/results/aggregate
 
 
-paths=$(realpath data/seidr/results/*/*.sf)
-sbatch pipeline/seidr-aggegate-micro.sh -f data/seidr/results/aggregate $paths
+paths=$(realpath data/seidr/results/*/*.sf | grep -v aggregated.sf)
+sbatch /mnt/picea/home/schoudhary/shruti/ShortTermNitrateTimeSeries-SNRIV/pipeline/seidr-aggregate-micro.sh -f data/seidr/results/aggregate $paths
 
