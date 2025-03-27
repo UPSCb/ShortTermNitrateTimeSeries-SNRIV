e906fe9baaee0efd3dfbce339a9fa65e6af1970c
EdoardoPiombo <edoardo.piombo@gmail.com>
2025-03-27 16:31:16 +0100

    Imoproved seidr view script

--- pipeline/seidr-view.sh
+++ pipeline/seidr-view.sh
@@ -3,11 +3,13 @@
 #SBATCH -p main -n 1
 #SBATCH --mem=16GB
 #SBATCH -A u2022003
+#SBATCH --output data/seidr/results/seidr-view.log
+#SBATCH --error data/seidr/results/seidr-view.err
 
 singularity=$(realpath singularity/seidr_0.14.2.sif)
 
 
 for thres in 1 5 9
 do singularity exec -B /mnt:/mnt $singularity seidr view --column-headers \
-data/seidr/results/backbone/backbone-"$thres"-percent.sf > data/seidr/results/backbone/backbone-"$thres"-percent.tsv
+data/seidr/results/backbone/backbone-"$thres"-percent.sf -o data/seidr/results/backbone/backbone-"$thres"-percent.tsv
 done
