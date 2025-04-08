8d4f8c089074ebd735a761f888b7687f6ae6c21e
fai-k <thee.kochakarn@gmail.com>
2025-04-08 15:43:00 +0200

    edit filter command

--- pipeline/seidr-view_filtering_HardThreshold.sh
+++ pipeline/seidr-view_filtering_HardThreshold.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -l
 #SBATCH -t 12:00:00
-#SBATCH -p main -n 20
+#SBATCH -p main -n 1
 #SBATCH --mem=16GB
-#SBATCH -A u2022003
+#SBATCH -A u2023011
+#SBATCH --output ../data/seidr/results/seidr-view.log
+#SBATCH --error ../data/seidr/results/seidr-view.err
 
-singularity=$(realpath singularity/seidr_0.14.2.sif)
-in=$(realpath data/seidr/results/aggregate/aggregated.sf)
-out=$(realpath data/seidr/results/aggregate)
+singularity=/mnt/picea/storage/singularity/seidr_0.14.2.sif
+in=$(realpath ../data/seidr/results/aggregate/aggregated.sf)
+out=$(realpath ../data/seidr/results/HardThreshold)
 
 singularity exec -B /mnt:/mnt $singularity seidr view --column-headers \
---filter "irp >= 0.444" $in -o $out/filtered_0.444.tsv
+-t 0.444 $in -o $out/filtered_0444.tsv
