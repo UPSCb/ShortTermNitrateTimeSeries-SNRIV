8a594e8e05d6f9884f2e06488c78d5842d6b534c
Shruti Choudhary <shruti@b-cn1614.hpc2n.umu.se>
2025-04-15 14:32:50 +0200

    -make subgraphs

--- src/R/seidrCentrality.R
+++ src/R/seidrCentrality.R
@@ -3,9 +3,11 @@ library(igraph)
 library(dplyr)
 library(tidyverse)
 library(tidyr)
-library(purrr)
+library(Publish)
 library(tibble)
 library(readr)
+library(purrr)
+library(readxl)
  
 bb9 <- read_tsv(here("data/seidr/clustering/filtered_backbone-9-percent.tsv"),
                col_names=T,col_types=cols(.default=col_character()),
@@ -18,87 +20,108 @@ bb9 <- bb9 %>%
 # have been already filtered out before, still run this
 bb9 <- bb9 %>% filter(!is.na(irp_score))
 
-# d.graf <- graph.edgelist(as.matrix(bb9[bb9$Type=="Directed",1:2]),directed=TRUE)
-
-# u1.graf <- graph.edgelist(as.matrix(bb9[bb9$Type=="Undirected",1:2]),directed=TRUE)
-# u2.graf <- graph.edgelist(as.matrix(bb9[bb9$Type=="Undirected",2:1]),directed=TRUE)
-# g <- union(d.graf,u1.graf,u2.graf)
-
-g <- graph.edgelist(as.matrix(bb9[bb9$Type=="Directed",1:2]),directed=TRUE)
+d.graf <- graph_from_edgelist(as.matrix(bb9[bb9$Type=="Directed",1:2]),directed=TRUE)
+u1.graf <- graph_from_edgelist(as.matrix(bb9[bb9$Type=="Undirected",1:2]),directed=TRUE)
+u2.graf <- graph_from_edgelist(as.matrix(bb9[bb9$Type=="Undirected",2:1]),directed=TRUE)
+g <- igraph::union(d.graf,u1.graf,u2.graf)
+saveRDS(g,"data/seidr/clustering/graph.rds")
 
 pr <- page_rank(g)$vector
-pr
+saveRDS(pr,"data/seidr/clustering/pageRank.rds")
+
+# Run this once you go through the tutorial, example:
+# https://dshizuka.github.io/networkanalysis/index.html
 
 # IRP = inverse of weights
-E(g)$weight <- bb9$irp_score
+# E(g)$weight <- bb9$irp_score
+# 
+# #Check that we do not need to use "-"
+# E(g)$inv_weight<- 1 / E(g)$weight
 
-#Check that we do not need to use "-"
-E(g)$inv_weight<- 1 / E(g)$weight
+#   in_degree = degree(g, mode = "in"),
+#   out_degree = degree(g, mode = "out"),
+#   total_degree = degree(g, mode = "all"),
+#   strength_in = strength(g, mode = "in", weights = E(g)$weight),
+#   strength_out = strength(g, mode = "out", weights = E(g)$weight),
+#   betweenness = betweenness(g, directed = T, weights = E(g)$inv_weight, normalized = T),
+#   closeness_in = closeness(g, mode = "in", weights = E(g)$inv_weight, normalized = T),
+#   closeness_out = closeness(g, mode = "out", weights = E(g)$inv_weight, normalized = T),
+#   eigenvector = eigen_centrality(g, directed = TRUE, weights = E(g)$weight)$vector 
 
+# Degree centrality =number of edges connected to a given node
+de = igraph::degree(g)
 
+# node strength=sum of the weights of edges connected to the node. 
+st= strength(g)
 
-# Following measures to calculate:
-# Degree= how many direct connections 
-# Strength=Weighted degree (e.g., based on IRP score)
-# Betweenness	= node lies on shortest paths or gene in between clusters (e.g. "switches"),
-# Closeness	of a node is to all others
-# Eigenvector	Node’s influence based on neighbor quality
-# PageRank? or strength to find highly connected genes (e.g. "modulators").
-#  any other?
+# betweenness centrality= number of shortest paths thorugh a node. Nodes with 
+# high betweenness might be influential in a network if, eg, they capture 
+# most amount of information # flowing through the network because information 
+# tends to flow through them.
+be=betweenness(g, normalized=T)
+saveRDS(be,"data/seidr/clustering/betweeness.rds")
 
-# Ask Nico to check this
-centrality <- data.frame(
-  node = V(g)$name,
-  in_degree = degree(g, mode = "in"),
-  out_degree = degree(g, mode = "out"),
-  total_degree = degree(g, mode = "all"),
-  strength_in = strength(g, mode = "in", weights = E(g)$weight),
-  strength_out = strength(g, mode = "out", weights = E(g)$weight),
-  
-  betweenness = betweenness(g, directed = T, weights = E(g)$inv_weight, normalized = T),
-  closeness_in = closeness(g, mode = "in", weights = E(g)$inv_weight, normalized = T),
-  closeness_out = closeness(g, mode = "out", weights = E(g)$inv_weight, normalized = T),
-  # Nico's function does not use the weights option for page rank.Should we use irp_score?
-  pagerank = page_rank(g, directed = TRUE, weights = E(g)$weight)$vector,
-  # optional
-  eigenvector = eigen_centrality(g, directed = TRUE, weights = E(g)$weight)$vector 
-  #there is a warning when I calculate with directed=T for eigencentrality, 
-)
+# Assemble the node-level measures 
+names=V(g)$name
+d=data.frame(node.name=names, degree=de, strength=st, betweenness=be, pagerank=pr) 
+saveRDS(d,"data/seidr/clustering/nodeMeasures.rds")
 
-first_neighbors_list <- lapply(V(g)$name, function(v) {
-  list(node = v, in_neighbors = names(neighbors(g, v, mode = "in")),
-       out_neighbors = names(neighbors(g, v, mode = "out")))
-})
+# first_neighbors_list <- lapply(V(g)$name, function(v) {
+#   list(node = v, in_neighbors = names(neighbors(g, v, mode = "in")),
+#        out_neighbors = names(neighbors(g, v, mode = "out")))})
+# 
+# neighbor_df <- map_dfr(first_neighbors_list, function(x) {
+#   tibble(node = x$node, direction = c(rep("in", length(x$in_neighbors)), 
+#                                       rep("out", length(x$out_neighbors))),
+#          neighbor = c(x$in_neighbors, x$out_neighbors))})
 
-write.csv(centrality, "data/seidr/clustering/bb9_centrality_directed.csv", row.names = FALSE)
-# write.csv(centrality, "data/seidr/clustering/bb9_centrality_undirected.csv", row.names = FALSE)
+# Resrtart R
+# different Timepoint is in separate sheet
+g <- readRDS("/pfs/stor10/users/home/s/shruti/project/ShortTermNitrateTimeSeries-SNRIV/data/seidr/clustering/graph.rds")
 
-neighbor_df <- map_dfr(first_neighbors_list, function(x) {
-  tibble(node = x$node, direction = c(rep("in", length(x$in_neighbors)), 
-                                      rep("out", length(x$out_neighbors))),
-         neighbor = c(x$in_neighbors, x$out_neighbors))})
+deg_file <- "data/deg/allDeg.xlsx"
+timepoint_map <- c("S1A_2h" = "2h", "S1B_4h" = "4h", "S1C_8h" = "8h", 
+                   "S1D_12h" = "12h")
 
+deg_split_list <- imap(timepoint_map, ~ {
+  df <- read_excel(deg_file, sheet = .y) %>%
+    select(Gene_Id, Log2_Fold_Change)
+  list(up = df %>% filter(Log2_Fold_Change > 0),
+    down = df %>% filter(Log2_Fold_Change < 0)
+    )
+}) %>% set_names(timepoint_map) 
 
-# Load DE genes for every Timepoint, separately
-DEG_by_T <- list()
-# goi <- sub("\\.1","",read.csv("DE-genes.csv",as.is = TRUE)[,1])
+#' Extract the first degree neighbours of DEGs genes from network
+# Use your dataframe or this
+# subgrafs <- map(subgraf <- make_ego_graph(g,1, get.vertex.attribute(graf,"name") %in% goi))
 
-#' Extract the first degree neighbours of these genes from the network
-#Use your dataframe or this
-subgrafs <- map(
-subgraf <- make_ego_graph(graf,1,
-                          get.vertex.attribute(graf,"name") %in% goi)
-)
+# get_neighbors <- function(goi, g) {
+#   ego_list <- make_ego_graph(g, order = 1, nodes = V(g)[name %in% goi])
+#   if (length(ego_list) == 0) return(make_empty_graph())
+#   combined_graph <- reduce(ego_list, igraph::union)
+#   return(combined_graph)
+# }
 
-walk(
-barplot(table(sapply(lapply(subgraf,clusters),"[[","csize")),
-        las=2,main="Gene of interest cluster size",
-        ylab="occurence",xlab="csize")
-)
+get_neighbors <- function(goi, g) {
+  ego_list <- make_ego_graph(g, order = 1, nodes = V(g)[name %in% goi])
+  ego_list <- unlist(ego_list) %>% unique()
+  return(ego_list)
+}
+
+deg_neighbors <- purrr::map(deg_split_list, ~ {
+  list(up_neighbors = get_neighbors(.x$up$Gene_Id, g),
+    down_neighbors = get_neighbors(.x$down$Gene_Id, g))})
+
+View(deg_neighbors)
+
+saveRDS(deg_neighbors,"data/seidr/clustering/deg_first_neighbors.rds")
+
+purrr::walk(barplot(table(sapply(lapply(deg_neighbors,clusters),"[[","csize")),
+        labs=2,main="DEG cluster size",ylab="occurence",xlab="csize"))
 
 #' combine all these networks together
 combined_networks <- map(
-fdn <- Reduce("%u%",subgraf)
+fdn <- Reduce("%u%",deg_neighbors)
 )
 
 #' Look at how many clusters we get and how many nodes are involved
