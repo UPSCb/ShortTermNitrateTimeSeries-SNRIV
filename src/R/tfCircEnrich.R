1aa83fa21ddc033ef7067de9727c63c2c2824f5b
fai-k <thee.kochakarn@gmail.com>
2025-05-15 14:26:51 +0200

    clean up code

--- src/R/tfCircEnrich.R
+++ src/R/tfCircEnrich.R
@@ -1,13 +1,9 @@
-library(stringr)
+library(tidyverse)
 library(here)
 library(igraph)
-library(dplyr)
-library(tidyverse)
 library(asnipe)
-library(purrr)
 library(readxl)
-library(tibble)
-library(readr)
+
 
 # circadian and TF enrichment in Clusters
 bb9 <- read_tsv(here("data/seidr/clustering/filtered_backbone-9-percent.tsv"),
@@ -19,9 +15,9 @@ cluster_info <- rbind(
   bb9[, c("Target", "Target_Cluster")] %>% setNames(c("geneID", "Cluster"))
 ) %>% distinct()
 
-circClock <- read_excel("data/nitResponsiveTFnCircadian.xlsx", sheet = 3)
-circReg <- read_excel("data/nitResponsiveTFnCircadian.xlsx", sheet = 4)
-TFgene <- read_excel("data/nitResponsiveTFnCircadian.xlsx", sheet = 2)
+circClock <- read_excel("data/enrichment/nitResponsiveTFnCircadian.xlsx", sheet = 3)
+circReg <- read_excel("data/enrichment/nitResponsiveTFnCircadian.xlsx", sheet = 4)
+TFgene <- read_excel("data/enrichment/nitResponsiveTFnCircadian.xlsx", sheet = 2)
 
 cluster_info_ext <- cluster_info %>%
   mutate(IsCircClock = geneID %in% circClock$Potra,
@@ -45,11 +41,10 @@ FisherCircClock <- map(unique(cluster_info$Cluster), function(c){
                   sum(!dat$IsCluster & dat$IsCircClock), 
                   sum(!dat$IsCluster & !dat$IsCircClock)), 
                 nrow = 2)
-  return(broom::tidy(fisher.test(mat)))
+  return(broom::tidy(fisher.test(mat)) %>% mutate(Cluster = c))
 }) %>%
   bind_rows() %>%
-  mutate(Cluster = unique(cluster_info$Cluster),
-         Sig = ifelse(p.value < 0.05, "Sig","ns"))
+  mutate(Sig = ifelse(p.value < 0.05, "Sig","ns"))
 
 FisherCircReg <- map(unique(cluster_info$Cluster), function(c){
   dat <- cluster_info_ext %>%
@@ -59,11 +54,10 @@ FisherCircReg <- map(unique(cluster_info$Cluster), function(c){
                   sum(!dat$IsCluster & dat$IsCircReg), 
                   sum(!dat$IsCluster & !dat$IsCircReg)), 
                 nrow = 2)
-  return(broom::tidy(fisher.test(mat)))
+  return(broom::tidy(fisher.test(mat)) %>% mutate(Cluster = c))
 }) %>%
   bind_rows() %>%
-  mutate(Cluster = unique(cluster_info$Cluster),
-         Sig = ifelse(p.value < 0.001, "Sig","ns"))
+  mutate(Sig = ifelse(p.value < 0.001, "Sig","ns"))
 
 FisherTF <- map(unique(cluster_info$Cluster), function(c){
   dat <- cluster_info_ext %>%
@@ -73,11 +67,10 @@ FisherTF <- map(unique(cluster_info$Cluster), function(c){
                   sum(!dat$IsCluster & dat$IsTF), 
                   sum(!dat$IsCluster & !dat$IsTF)), 
                 nrow = 2)
-  return(broom::tidy(fisher.test(mat)))
+  return(broom::tidy(fisher.test(mat)) %>% mutate(Cluster = c))
 }) %>%
   bind_rows() %>%
-  mutate(Cluster = unique(cluster_info$Cluster),
-         Sig = ifelse(p.value < 0.001, "Sig","ns"))
+  mutate(Sig = ifelse(p.value < 0.001, "Sig","ns"))
 
 write.table(FisherTF, "data/fisherTF.txt", quote = F, sep = "\t", 
             row.names = F, col.names = T)
